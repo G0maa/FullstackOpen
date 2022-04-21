c641354db77f5caea91e80eddcd83e1e395b4753
G0maa <midomaxgomaa@gmail.com>
2022-04-21 19:05:20 +0200

    1.4 done

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,26 +1,28 @@
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = {
-        name:'Fundamentals of React',
-        exercises: 10
-    }
-    const part2 = {
-        name:'Using props to pass data',
-        exercises: 7
-    }
-    const part3 = {
-        name: 'State of a component',
-        exercises: 14
-    }
+    const parts = [
+        {
+            name:'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name:'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+    ]
 
 
     // Could have looked a bit better I guess.
     return (
         <div>
             <Header course={course}/>
-            <Content parts={[part1, part2, part3]}/>
-            <Total parts={[part1, part2, part3]} />
+            <Content parts={parts}/>
+            <Total parts={parts} />
         </div>
     )
 }
