8b6a50e5b020b6c99c263e0a4b4bbbdab951c617
G0maa <midomaxgomaa@gmail.com>
2022-05-27 09:47:01 +0200

    P2B partCode

--- part2/partCode/src/App.js
+++ part2/partCode/src/App.js
@@ -1,18 +1,57 @@
-// import { useState } from "react"
+import { useState } from "react"
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+    // So... I am confused, when should we really use `useState()`?
+    // Why did we do all of this, weren't the native way easier?
+    // => Wait for submit, take value...
+    // <= Now we change state everytime we have a keystroke.
+    // How does `useState()` really work?
+    const [notes, setNotes] = useState(props.notes)
+    const [newNote, setNewNote] = useState('')
+    const [showAll, setShowAll] = useState(true)
+
+    const notesToShow = showAll ? notes : notes.filter(note => note.important)
+
+    const addNote = (event) => {
+        event.preventDefault()
+        const noteObject = {
+            content: newNote,
+            date: new Date().toISOString(),
+            important: Math.random() < 0.5,
+            id: notes.length + 1
+        }
+
+        setNotes(notes.concat(noteObject))
+        setNewNote('')
+    }
+
+    const handleNoteChange = (event) => {
+        console.log(event.target.value)
+        setNewNote(event.target.value)
+    }
+
     return(
         <div>
             <h1>Notes</h1>
+            <div>
+                <button onClick={() => setShowAll(!showAll)}>
+                    show {showAll ? 'important' : 'all'}
+                </button>
+            </div>
             <ul>
-                {notes.map(note =>
+                {notesToShow.map(note =>
                     <Note key={note.id} note={note} />
                 )}
             </ul>
+            <form onSubmit={addNote}>
+                <input value={newNote}
+                 onChange={handleNoteChange}
+                />
+                <button type="submit">save</button>
+            </form>
         </div>
     )
-
 }
 
 export default App
