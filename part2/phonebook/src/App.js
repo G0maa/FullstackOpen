7949ebf9218f52739f05885a939fd4fb60ad741c
Noriel <N/A>
2022-05-27 16:21:23 +0200

    P2B Ex 2.6 Done

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const onChangeName = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+
+    // if(perso)
+
+    setPersons(persons.concat({name: newName}))
+  }
+
+  // Probably should refactor this code.
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={onChangeName} />
+        </div>
+        <div>
+          <button type="submit" >add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((element, idx) => <li key={idx}>{element.name}</li>)}
+    </div>
+  )
+}
+
+export default App
