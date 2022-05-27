ffc6da87bee1e5e6bf32e0611b52c3fe2b7fb1eb
G0maa <midomaxgomaa@gmail.com>
2022-05-27 17:35:07 +0200

    P2B Ex 2.9 Done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,16 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState(0)
+  const [newNumber, setNewNumber] = useState('')
+  const [searchFilter, setSearchFilter] = useState('')
+
 
   const onChangeName = (event) => {
     setNewName(event.target.value)
@@ -14,6 +20,13 @@ const App = () => {
   const onChangeNumber = (event) => {
     setNewNumber(event.target.value)
   }
+
+  const onChangeFilter = (event) => {
+    setSearchFilter(event.target.value)
+  }
+
+  // small addition.
+  const filteredPersons = (searchFilter === '') ? persons : persons.filter((element) => element.name.toLowerCase().match(searchFilter.toLowerCase()))
 
   const addName = (event) => {
     event.preventDefault()
@@ -24,13 +37,15 @@ const App = () => {
       return
     }
 
-    setPersons(persons.concat({name: newName, number:newNumber}))
+    setPersons(persons.concat({name: newName, number:newNumber, id:persons.length + 1}))
   }
 
   // Probably should refactor this code.
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>Fitler shown with: <input value={searchFilter} onChange={onChangeFilter}/></div>
+      <h2>Add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={onChangeName} />
@@ -43,7 +58,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((element, idx) => <li key={idx}>{element.name}, {element.number}</li>)}
+      {filteredPersons.map((element) => <li key={element.id}>{element.name}, {element.number}</li>)}
     </div>
   )
 }
