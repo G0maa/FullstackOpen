a3d9135117e82d04d4b2c849926993641ad4d078
G0maa <midomaxgomaa@gmail.com>
2022-05-31 17:21:45 +0200

    P2C Ex 2.11 Done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Filter from './Components/Filter'
 import PersonForm from './Components/PersonForm'
@@ -6,16 +7,25 @@ import Persons from './Components/Persons'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchFilter, setSearchFilter] = useState('')
+
+  // New bit
+  useEffect(() => {
+    console.log("Effect")
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => {
+        console.log("Response of XHR")
+        setPersons(response.data)
+      })
+
+    console.log("After XHR request")
+  }, [])
+  console.log(`Rendered persons ${persons.length} length`)
 
 
   const onChangeName = (event) => {
