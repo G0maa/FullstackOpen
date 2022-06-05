a56dfbd2231fe4d1f18fb2ab6dd3321241fbcd26
Noriel <N/A>
2022-06-05 20:54:49 +0200

    P2D Ex 2.17 Done

--- part2/phonebook/src/Components/Persons.js
+++ part2/phonebook/src/Components/Persons.js
@@ -1,16 +1,23 @@
 import React from "react" 
 
-const Persons = ({filteredPersonsArr}) => {
+const Persons = ({filteredPersonsArr, onDeleteFunc}) => {
     return(
         <>
-        {filteredPersonsArr.map((element) => <Person key={element.id} person={element} />)}
+            {filteredPersonsArr.map((element) => <Person key={element.id} person={element} onDeleteFunc={onDeleteFunc} />)}
         </>
     )
 }
 
-const Person = ({person}) => {
+const Person = ({person, onDeleteFunc}) => {
+    // I guess the `onDleteFunc()` function can be moved here.
+
+    const buttonClick = () => {
+        if(window.confirm(`Delete "${person.name}"?`))
+            onDeleteFunc(person.id)
+    }
+
     return (
-        <li key={person.id}>{person.name}, {person.number}</li>
+        <li key={person.id}>{person.name}, {person.number} <button onClick={buttonClick}>delete</button></li>
     )
 }
 
