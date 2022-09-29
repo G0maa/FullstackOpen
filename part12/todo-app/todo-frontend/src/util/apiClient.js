e2467855e277517a42ea99db9e87b09111e28f35
G0maa <midomaxgomaa@gmail.com>
2022-09-29 11:06:44 +0200

    P12C - Ex 12.15 to 12.19 Done

--- part12/todo-app/todo-frontend/src/util/apiClient.js
+++ part12/todo-app/todo-frontend/src/util/apiClient.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  // baseURL: process.env.REACT_APP_BACKEND_URL,
-  baseURL: "http://localhost:3000/",
+  baseURL: process.env.REACT_APP_BACKEND_URL,
 });
 
 export default apiClient;
